d741b713f2e5808f89b61409a793e45307295b15
anmolSharma <sharmaanmol313@gmail.com>
2022-05-28 19:44:40 +0530

    added animation in the tabbar

--- lib/home/main_home_page.dart
+++ lib/home/main_home_page.dart
@@ -1,28 +1,90 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:hearooz/home/header_sub/circular_main_page_list_view.dart';
-import 'package:hearooz/home/header_sub/main_page_list_view.dart';
-import 'package:hearooz/home/header_sub/title_row.dart';
 import 'package:hearooz/home/main_header.dart';
 import 'package:hearooz/home/widgets/heart_icon_screen.dart';
 import 'package:hearooz/home/widgets/home_icon_screen.dart';
 import 'package:hearooz/home/widgets/profile_icon.dart';
 import 'package:hearooz/home/widgets/search_icon_screen.dart';
-import 'package:hearooz/home/widgets/side_container.dart';
-import 'package:hearooz/utils/colors.dart';
 
 class MainHomePage extends StatefulWidget {
   @override
   State<MainHomePage> createState() => _MainHomePageState();
 }
 
-class _MainHomePageState extends State<MainHomePage> {
+class _MainHomePageState extends State<MainHomePage>
+    with TickerProviderStateMixin {
   bool selected = false;
+  bool isheartSet = false;
+  int _selectedIndex = 0;
+  late TabController _tabController;
+
+  late AnimationController _homeAnimationController;
+  late Animation _homeAnimation;
+
+  late AnimationController _heartAnimationController;
+  late Animation _heartAnimation;
+
+  late AnimationController _searchAnimationController;
+  late Animation _searchAnimation;
+
+  late AnimationController _profileAnimationController;
+  late Animation _profileAnimation;
+
   @override
   void initState() {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    _tabController = TabController(length: 4, vsync: this, initialIndex: 0);
+
+    _homeAnimationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 80));
+    _homeAnimation = Tween(begin: 30.0, end: 40.0).animate(CurvedAnimation(
+        curve: Curves.bounceOut, parent: _homeAnimationController));
+
+    _heartAnimationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 80));
+    _heartAnimation = Tween(begin: 30.0, end: 40.0).animate(CurvedAnimation(
+        curve: Curves.bounceOut, parent: _heartAnimationController));
+
+    _searchAnimationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 80));
+    _searchAnimation = Tween(begin: 30.0, end: 40.0).animate(CurvedAnimation(
+        curve: Curves.bounceOut, parent: _searchAnimationController));
+
+    _profileAnimationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 80));
+    _profileAnimation = Tween(begin: 30.0, end: 40.0).animate(CurvedAnimation(
+        curve: Curves.bounceOut, parent: _profileAnimationController));
+
+    _tabController.addListener(() {
+      setState(() {
+        _selectedIndex = _tabController.index;
+
+        if (_selectedIndex == 0) {
+          _homeAnimationController.forward();
+        }
+
+        if (_selectedIndex == 1) {
+          _heartAnimationController.forward();
+        }
+
+        if (_selectedIndex == 2) {
+          _searchAnimationController.forward();
+        }
+
+        if (_selectedIndex == 3) {
+          _profileAnimationController.forward();
+        }
+      });
+    });
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _heartAnimationController.dispose();
+    _tabController.dispose();
   }
 
   @override
@@ -59,11 +121,12 @@ class _MainHomePageState extends State<MainHomePage> {
                     ),
                   ),
                 ),
-                const Expanded(
+                Expanded(
                   flex: 4,
                   child: SizedBox(
                     height: 50,
                     child: TabBar(
+                      controller: _tabController,
                       indicatorColor: Colors.transparent,
                       padding: EdgeInsets.zero,
                       indicatorPadding: EdgeInsets.zero,
@@ -72,31 +135,45 @@ class _MainHomePageState extends State<MainHomePage> {
                         Tab(
                           icon: Icon(
                             Icons.home,
-                            size: 30,
-                            color: Colors.white,
+                            size:
+                                _selectedIndex == 0 ? _homeAnimation.value : 30,
+                            color: _selectedIndex == 0
+                                ? Colors.yellow
+                                : Colors.white,
                           ),
                         ),
                         Tab(
-                          icon: Icon(
+                          child: Icon(
                             CupertinoIcons.heart_fill,
-                            size: 30,
-                            color: Colors.white,
+                            size: _selectedIndex == 1
+                                ? _heartAnimation.value
+                                : 30,
+                            color:
+                                _selectedIndex == 1 ? Colors.red : Colors.white,
                           ),
                         ),
                         Tab(
                           icon: Icon(
                             CupertinoIcons.search,
-                            size: 30,
-                            color: Colors.white,
+                            size: _selectedIndex == 2
+                                ? _searchAnimation.value
+                                : 30,
+                            color: _selectedIndex == 2
+                                ? Colors.yellow
+                                : Colors.white,
                           ),
                         ),
                         Tab(
                           child: Padding(
-                            padding: EdgeInsets.only(left: 15.0),
+                            padding: const EdgeInsets.only(left: 15.0),
                             child: Icon(
                               CupertinoIcons.profile_circled,
-                              size: 30,
-                              color: Colors.white,
+                              size: _selectedIndex == 3
+                                  ? _profileAnimation.value
+                                  : 30,
+                              color: _selectedIndex == 3
+                                  ? Colors.yellow
+                                  : Colors.white,
                             ),
                           ),
                         )
@@ -200,9 +277,10 @@ class _MainHomePageState extends State<MainHomePage> {
               ),
               Container(
                 height: 700,
-                child: const TabBarView(
-                    physics: NeverScrollableScrollPhysics(),
-                    children: [
+                child: TabBarView(
+                    controller: _tabController,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: const [
                       HomeIconScreen(),
                       HeartIconScreen(),
                       SearchIconScreen(),
